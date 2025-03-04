2357ab3741f8b91f5bc6825910db8953d4d9a90c
ArmanKolozyan <92608959+ArmanKolozyan@users.noreply.github.com>
2025-03-04 11:55:58 +0100

    test: added test for ite

---
+++ test/IteParseTest.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+
+module IteParseTest where
+
+import Syntax.Compiler (parseAndCompile)
+import Syntax.AST        
+
+testFile :: String
+testFile = "testFiles/ite.circir"
+
+runIteParsingTest :: IO Bool
+runIteParsingTest = do
+    content <- readFile testFile
+    case parseAndCompile content of
+      Left err -> do
+          putStrLn $ "Parsing or compilation failed with error: " ++ err
+          return False
+      Right program -> do
+          -- we want to confirm that the Program’s computations contain 
+          -- an 'Ite (Var \"sel\") (Var \"a\") (Var \"b\")' expression.
+          let expected = Ite (Var "sel") (Var "a") (Var "b")
+              actualComputations = computations program
+
+          if any (== expected) actualComputations
+            then do
+              putStrLn "SUCCESS: Found Ite (Var \"sel\") (Var \"a\") (Var \"b\") in computations."
+              return True
+            else do
+              putStrLn "FAILURE: Did not find the expected Ite expression in computations."
+              putStrLn $ "Got: " ++ show actualComputations
+              return False
