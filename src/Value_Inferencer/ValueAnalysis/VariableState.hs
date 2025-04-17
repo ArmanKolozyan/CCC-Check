a79b74e8e6fe70fe947b411b71b294fdb9bbaf85
ArmanKolozyan <92608959+ArmanKolozyan@users.noreply.github.com>
2025-04-17 23:08:44 +0200

    moved some functions to VariableState file

--- src/Value_Inferencer/ValueAnalysis/VariableState.hs
+++ src/Value_Inferencer/ValueAnalysis/VariableState.hs
@@ -2,6 +2,9 @@ module ValueAnalysis.VariableState where
 
 
 import ValueAnalysis.ValueDomain (ValueDomain, defaultValueDomain)
+import Syntax.AST
+import Data.Map
+import qualified Data.Map as Map
 
 -- | Tracks the state of each variable.
 newtype VariableState = VariableState {domain :: ValueDomain} deriving (Eq, Show)
@@ -9,3 +12,32 @@ newtype VariableState = VariableState {domain :: ValueDomain} deriving (Eq, Show
 -- | Initial VariableState (no restrictions at the beginning).
 initVarState :: VariableState
 initVarState = VariableState { domain = defaultValueDomain }
+
+-- | Builds a map from variable IDs to their initial state.
+initializeVarStates :: [Binding] -> Map Int VariableState
+initializeVarStates vars = Map.fromList [(vid v, initVarState) | v <- vars]
+
+-- | Builds a map from variable names to their IDs for lookup.
+-- TODO: just replace all vars in constraints by their ID during compilation!
+buildVarNameToIDMap :: [Binding] -> Map String Int
+buildVarNameToIDMap vars = Map.fromList [(name v, vid v) | v <- vars]
+
+-- | Lookup variable ID by name.
+lookupVarID :: String -> Map String Int -> Either String Int
+lookupVarID name nameToID =
+  case Map.lookup name nameToID of
+    Just vID -> Right vID
+    Nothing  -> Left $ "Variable name not found in nameToID map: " ++ name
+
+-- | Lookup variable state by ID.
+lookupVarState :: Int -> Map Int VariableState -> Either String VariableState
+lookupVarState vID varStates =
+  case Map.lookup vID varStates of
+    Just state -> Right state
+    Nothing    -> Left $ "Variable state not found in varStates for ID: " ++ show vID
+
+-- | Lookup variable state by name.
+lookupVarStateByName :: String -> Map String Int -> Map Int VariableState -> Either String VariableState
+lookupVarStateByName name nameToID varStates = do
+  vID <- lookupVarID name nameToID
+  lookupVarState vID varStates
