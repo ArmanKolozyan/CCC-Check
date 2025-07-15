25dfc5d066ebfca84adafea3e2585fabb8e5ebed
ArmanKolozyan <92608959+ArmanKolozyan@users.noreply.github.com>
2025-07-15 13:16:31 +0200

    test: updated testRunner

--- test/TestRunner.hs
+++ test/TestRunner.hs
@@ -27,7 +27,7 @@ import qualified Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestBinary
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestNoOutputTag
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.BinSubTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.BinSubTestDeep
-import qualified Value_Inferencer.BugDetection.TaggedCircomlib.SumTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.BinSumTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.SumTestDeep
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.NotTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.XorTest
@@ -35,7 +35,20 @@ import qualified Value_Inferencer.BugDetection.TaggedCircomlib.AndTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.OrTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.NandTest
 import qualified Value_Inferencer.BugDetection.TaggedCircomlib.NorTest
-
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.IsZeroTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.IsZeroForcedTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.IsEqualTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.IsEqualForcedTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.Num2BitsTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.Bits2NumTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.LessThanTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.GreaterThanTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.GreaterEqThanTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.LessEqThanTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.BigLessThanTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.ModSumTest
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.MultiMux1Test
+import qualified Value_Inferencer.BugDetection.TaggedCircomlib.Mux1Test
 
 main :: IO ()
 main = hspec $ do
@@ -60,13 +73,11 @@ main = hspec $ do
     Value_Inferencer.BugDetection.Arrays.ArrayTagTest.spec
     Value_Inferencer.BugDetection.Arrays.ArraySelectOutOfBoundsTest.spec
     Value_Inferencer.BugDetection.Arrays.ArrayStoreOutOfBoundsTest.spec
-    Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestRaw.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestBinary.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestNoOutputTag.spec
-    Value_Inferencer.BugDetection.TaggedCircomlib.BinSubTest.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.BinSubTestDeep.spec
-    Value_Inferencer.BugDetection.TaggedCircomlib.SumTest.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.SumTestDeep.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.ModSumTest.spec
 
     Value_Inferencer.BugDetection.TaggedCircomlib.NotTest.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.XorTest.spec
@@ -74,4 +85,21 @@ main = hspec $ do
     Value_Inferencer.BugDetection.TaggedCircomlib.OrTest.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.NandTest.spec
     Value_Inferencer.BugDetection.TaggedCircomlib.NorTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.IsZeroTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.IsZeroForcedTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.DecoderTestRaw.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.IsEqualTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.IsEqualForcedTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.Num2BitsTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.Bits2NumTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.MultiMux1Test.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.Mux1Test.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.LessThanTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.GreaterThanTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.GreaterEqThanTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.LessEqThanTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.BigLessThanTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.BinSubTest.spec
+    Value_Inferencer.BugDetection.TaggedCircomlib.BinSumTest.spec
+
 
